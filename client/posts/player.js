3ef57f9d06318d51868d99076309bb4af0981f1c
sandman18 <robertsand@Roberts-MacBook-Pro-2.local>
2013-06-18 15:15:57 -0400

    Updated files

---
+++ client/posts/player.js
@@ -0,0 +1,46 @@
+Template.video.rendered = function () {
+	jwplayer('my-video').setup({
+		file: 'http://localhost:3000/01TodayIsTheDay.mp3',
+		height: '25'
+	});
+};
+
+
+Template.youtube.rendered = function () {
+    jwplayer('kenpo').setup({
+        file: 'http://youtu.be/jhHJGHQlE-0',
+        width: '640',
+        height: '360'
+    });
+};
+
+ 
+
+function mp3meta() {
+
+ function test1() {
+	var file = "http://localhost:3000/01TodayIsTheDay.mp3";
+
+	var out = document.getElementById("output");
+	out.value = "Loading file \"" + file + "\"..\r\n";
+
+	function callback() {
+		out.value += "Getting all tags\r\n";
+
+		var tags = ID3.getAllTags(file);
+
+		out.value += "Found: \r\n"
+			+ "artist: " + tags["artist"] + "\r\n"
+			+ "title: " + tags["title"] + "\r\n"
+			+ "album: " + tags["album"] + "\r\n"
+			+ "year: " + tags["year"] + "\r\n"
+			+ "comment: " + tags["comment"] + "\r\n"
+			+ "genre: " + tags["genre"] + "\r\n"
+			+ "track: " + tags["track"] + "\r\n"
+	};
+
+	ID3.loadTags(file, callback);
+
+};
+
+};
